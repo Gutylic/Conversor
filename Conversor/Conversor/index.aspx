2b3797bb6d1e099e5acfb7c6f0b094abc3f03383
Gustavo <Licenciados@outlook.com.ar>
2016-07-07 00:24:42 -0300

    Conversor

---
+++ Conversor/Conversor/index.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="Conversor.index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:FileUpload ID="FileUpload_Ejercicio" runat="server" />
+
+        <br />
+        <br />
+        <asp:Button ID="BtnConversor" runat="server" Text="Convertir" OnClick="BtnConversor_Click" Width="601px" />
+        <br />
+        <br />
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; ===========&gt; crear una carpeta en c:/corregido/ &lt;============</div>
+    </form>
+</body>
+</html>
---
+++ Conversor/Conversor/index.aspx.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+
+namespace Conversor
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void BtnConversor_Click(object sender, EventArgs e)
+        {
+            if (!FileUpload_Ejercicio.HasFile)
+            {
+                string Mensaje = @"<script type='text/javascript'>   
+                                   alert('Es obligatorio cargar un archivo');
+                                   </script>";
+                ScriptManager.RegisterStartupScript(this, typeof(Page), "", Mensaje, false);
+                return;
+            }
+
+            Session["fileName"] = FileUpload_Ejercicio.FileName;
+
+            string filename = Path.Combine(Server.MapPath("~/corregido"), (string)Session["fileName"]);
+            FileUpload_Ejercicio.SaveAs(filename);
+            StreamReader file = new StreamReader(filename, System.Text.Encoding.UTF8);
+            
+            
+            string texto = file.ReadToEnd();
+            file.Close();
+            
+            
+            Session["Datos"] = texto;
+            string[] datos = Session["Datos"].ToString().Split('╝');
+            Session["Enunciado"] = datos[0];
+            Session["Enunciado_Limpio"] = datos[1];
+            Session["Titulo"] = datos[2];
+            Session["Tipo_Producto"] = datos[4];
+            Session["Realizada_Explicacion"] = datos[8];
+            Session["Ejercicio"] = datos[5];
+            Session["Video"] = datos[7];
+
+
+            StreamWriter Archivo = File.CreateText("C:\\corregido/" + FileUpload_Ejercicio.FileName); // carga el archivo corregido del enunciado en c:
+
+            Archivo.Write((string)Session["Enunciado"]);
+            Archivo.Write("╝");
+            Archivo.Write((string)Session["Enunciado_Limpio"]);
+            Archivo.Write("╝");
+            Archivo.Write((string)Session["Titulo"]);
+            Archivo.Write("╝");
+            Archivo.Write((string)Session["Tipo_Producto"]);
+            Archivo.Write("╝");
+            Archivo.Write((string)Session["Video"]);
+            Archivo.Write("╝");
+            Archivo.Write((string)Session["Realizada_Explicacion"]);
+            Archivo.Write("╝");
+            Archivo.Write((string)Session["Ejercicio"]);
+            Archivo.Write("╝");
+            
+            Archivo.Flush();
+            Archivo.Close();
+
+
+
+        }
+    }
+}
